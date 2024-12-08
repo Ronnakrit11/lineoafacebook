7c89fc4109cc6a0b4f5bf80827bb4fa37b256037
21 Tech Enterprise <59superrichsol@gmail.com>
2024-12-09 01:16:43 +0700

    done

--- lib/messageFormatter.ts
+++ lib/messageFormatter.ts
@@ -1,4 +1,4 @@
-import { Message, Conversation } from '@prisma/client';
+import { Message } from '@prisma/client';
 import type { ConversationWithMessages } from '@/app/types/chat';
 
 export interface PusherMessage {
