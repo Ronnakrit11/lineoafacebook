c236394575a9514da69c33aac40c83d3d1e47f1e
21 Tech Enterprise <59superrichsol@gmail.com>
2024-12-08 22:39:14 +0700

    done

--- app/types/global.d.ts
+++ app/types/global.d.ts
@@ -1,6 +1,8 @@
 import type { SocketServer } from './socket';
 
 declare global {
+  var io: SocketServer | undefined;
+  
   namespace NodeJS {
     interface Global {
       io: SocketServer | undefined;
