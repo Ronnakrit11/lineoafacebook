aae16331eece235ba744721166afca2331abc4f5
21 Tech Enterprise <59superrichsol@gmail.com>
2024-12-08 21:24:17 +0700

    first commit

--- app/components/ChatInterface.tsx
+++ app/components/ChatInterface.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { ConversationWithMessages } from '../types/chat';
 import { ConversationList } from './ConversationList';
 import { MessageList } from './MessageList';
